971edfeee4b35c8c55d6a019b0d1214ebfd8d812
Hanif Fakira <fakira.work@gmail.com>
2020-12-27 14:09:04 +0000

    Using yatsepc test log for assertign on http response
    Signed-off-by: Hanif Fakira <fakira.work@gmail.com>

--- src/test/java/acceptancetests/_01reqandresponly/tests/UsecaseOneMoreExamplesTest.java
+++ src/test/java/acceptancetests/_01reqandresponly/tests/UsecaseOneMoreExamplesTest.java
@@ -1,15 +1,18 @@
 package acceptancetests._01reqandresponly.tests;
 
 import acceptancetests._01reqandresponly.testinfrastructure.AcceptanceTest;
+import acceptancetests._01reqandresponly.thens.ThenTheResponseVersion3;
 import com.googlecode.yatspec.junit.Notes;
 import com.googlecode.yatspec.junit.WithParticipants;
 import com.googlecode.yatspec.sequence.Participant;
 import org.junit.jupiter.api.Test;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 import static acceptancetests._01reqandresponly.testinfrastructure.YatspecConstants.APP_PARTICIPANT;
 import static acceptancetests._01reqandresponly.testinfrastructure.YatspecConstants.CLIENT_ACTOR;
+import static acceptancetests._01reqandresponly.testinfrastructure.YatspecConstants.RESPONSE_FROM_APP_TO_CLIENT;
 
 @SuppressWarnings("SameParameterValue") // For test readability
 class UsecaseOneMoreExamplesTest extends AcceptanceTest implements WithParticipants {
@@ -37,6 +40,24 @@ class UsecaseOneMoreExamplesTest extends AcceptanceTest implements WithParticipa
         .hasStatusCode(200)
         .hasContentType("text/html")
         .hasBody("Hello, World");
+  }
+
+  @Test
+  @Notes("Using a response stored in yatsepc test log")
+  void shouldReturnResponseVersionThree() throws Exception {
+    whenARequest
+        .withUri("http://localhost:2222/usecaseone")
+        .isCalledUsingHttpGetMethod();
+
+    thenResponseReceived()
+        .hasStatusCode(200)
+        .hasContentType("text/html")
+        .hasBody("Hello, World");
+  }
+
+  private ThenTheResponseVersion3 thenResponseReceived() {
+    HttpResponse<String> response =  testState.getType(RESPONSE_FROM_APP_TO_CLIENT, HttpResponse.class);
+    return new ThenTheResponseVersion3(testState, response);
   }
 
   @Override
---
+++ src/test/java/acceptancetests/_01reqandresponly/thens/ThenTheResponseVersion3.java
@@ -0,0 +1,32 @@
+package acceptancetests._01reqandresponly.thens;
+
+import com.googlecode.yatspec.state.givenwhenthen.TestState;
+
+import java.net.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThenTheResponseVersion3 {
+  private final TestState testState;
+  private final HttpResponse<String> response;
+
+  public ThenTheResponseVersion3(TestState testState, HttpResponse<String> response) {
+    this.testState = testState;
+    this.response = response;
+  }
+
+  public ThenTheResponseVersion3 hasStatusCode(int expectedStatusCode) {
+    assertThat(response.statusCode()).isEqualTo(expectedStatusCode);
+    return this;
+  }
+
+  public ThenTheResponseVersion3 hasContentType(String expectedContentType) {
+    assertThat(response.headers().firstValue("content-type")).contains(expectedContentType);
+    return this;
+  }
+
+  public ThenTheResponseVersion3 hasBody(String expectedBody) {
+    assertThat(response.body()).isEqualTo(expectedBody);
+    return this;
+  }
+}
